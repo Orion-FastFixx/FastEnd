05df4602bc64efc83e3eaae8f9ccbb28034052f2
Rayhan Illyas <rayhan205732@gmail.com>
2023-11-09 23:19:09 +0700

    init backend services

---
+++ routes/index.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from 'express';
+
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req: Request, res: Response, next: NextFunction) {
+  res.json({ title: 'FastEnd services for FastFixx Horee' });
+});
+
+module.exports = router;
