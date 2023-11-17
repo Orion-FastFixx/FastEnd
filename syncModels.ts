bd6fc833579469eb0b60ddafac48c5b0f5f615cc
Rayhan Illyas <rayhan205732@gmail.com>
2023-11-17 19:53:20 +0700

    feat: add admin models

--- syncModels.ts
+++ syncModels.ts
@@ -5,6 +5,7 @@ import Pengendara from "./app/models/pengendara.models";
 import Kendaraan from "./app/models/kendaraan.models";
 import Bengkel from "./app/models/bengkel.models";
 import Rating from "./app/models/rating.models";
+import Admin from "./app/models/admin.models";
 
 console.log('Is Role model registered:', Role === sequelize.model('roles'));
 console.log('Is Kendaraan model registered:', Kendaraan === sequelize.model('kendaraans'));
@@ -12,6 +13,7 @@ console.log('Is User model registered:', User === sequelize.model('users'));
 console.log('Is Pengendara model registered:', Pengendara === sequelize.model('pengendaras'));
 console.log('Is Rating model registered:', Rating === sequelize.model('ratings'));
 console.log('Is Bengkel model registered:', Bengkel === sequelize.model('bengkels'));
+console.log('Is Admin model registered:', Admin === sequelize.model('admins'));
 
 sequelize.sync({ force: true }) // Set force to true if you want to drop the tables first
     .then(() => {
