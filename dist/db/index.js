3ce6a24d566986a460c895d970d0410fec4b045e
Rayhan Illyas <rayhan205732@gmail.com>
2023-11-17 11:21:53 +0700

    refactor: change connection from mongo to sequelize mysql2

--- dist/db/index.js
+++ dist/db/index.js
@@ -1,17 +1,33 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = void 0;
-const mongoose_1 = __importDefault(require("mongoose"));
+exports.connectDb = exports.sequelize = void 0;
 const config_1 = require("../app/config");
-const initRole_controller_1 = require("../app/controllers/initRole.controller");
-mongoose_1.default.connect(config_1.config.urlDb)
-    .then(() => {
-    console.log('MongoDB connected...');
-    (0, initRole_controller_1.initializeRoles)();
-})
-    .catch((err) => console.log("Connection error: ", err));
-exports.db = mongoose_1.default.connection;
+const sequelize_1 = require("sequelize");
+// Initialize a new Sequelize instance
+const sequelize = new sequelize_1.Sequelize(config_1.config.mysqlDatabase, config_1.config.mysqlUser, config_1.config.mysqlPassword, {
+    host: config_1.config.mysqlHost,
+    dialect: 'mysql',
+    port: config_1.config.mysqlPort,
+});
+exports.sequelize = sequelize;
+const connectDb = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    }
+    catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+});
+exports.connectDb = connectDb;
+connectDb();
 //# sourceMappingURL=index.js.map
