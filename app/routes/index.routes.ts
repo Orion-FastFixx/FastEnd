4f067f3bfe294e853718a888d4abb487206a9078
Rayhan Illyas <rayhan205732@gmail.com>
2023-11-17 19:56:41 +0700

    refactor: config auth and init role routes

--- app/routes/index.routes.ts
+++ app/routes/index.routes.ts
@@ -1,7 +1,12 @@
 import router from './user.routes';
 import userRoutes from './user.routes';
+import initRolesRoutes from './initRole.routes';
+import authenticationRoutes from './authentication.routes';
 
 router.use('/user', userRoutes);
+router.use('/auth', authenticationRoutes);
+router.use('/init-roles', initRolesRoutes);
+
 
 
 export const routes = router;
--- dist/app/routes/index.routes.js
+++ dist/app/routes/index.routes.js
@@ -6,6 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.routes = void 0;
 const user_routes_1 = __importDefault(require("./user.routes"));
 const user_routes_2 = __importDefault(require("./user.routes"));
+const initRole_routes_1 = __importDefault(require("./initRole.routes"));
+const authentication_routes_1 = __importDefault(require("./authentication.routes"));
 user_routes_1.default.use('/user', user_routes_2.default);
+user_routes_1.default.use('/auth', authentication_routes_1.default);
+user_routes_1.default.use('/init-roles', initRole_routes_1.default);
 exports.routes = user_routes_1.default;
 //# sourceMappingURL=index.routes.js.map
