86faf8e631f5d3db594c05498229a527f188798e
Rayhan Illyas <rayhan205732@gmail.com>
2023-12-05 21:08:26 +0700

    feat: add getList montir in admin controller

---
+++ app/controllers/admin.controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express"
+import User from "../models/user.models";
+import Montir from "../models/montir.models";
+
+export const AdminController = {
+
+    async getAllMontir(req: Request, res: Response) {
+        try {
+            const montirs = await User.findAll({
+                where: { role_id: 4 },
+            });
+
+            res.status(200).json({
+                message: "Success get all montir",
+                data: montirs
+            });
+
+        } catch (error: any) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+
+}
