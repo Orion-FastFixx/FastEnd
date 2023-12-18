7275584cc2b944bc9211c71ce9ae4fcfbc9821e1
muhammadikramjundulloh <92200158+muhammadikramjundulloh@users.noreply.github.com>
2023-12-18 13:02:39 +0700

    additional

--- syncModels.ts
+++ syncModels.ts
@@ -43,7 +43,7 @@ console.log('Is MontirService model registered:', MontirService === sequelize.mo
 console.log('Is Education model registered:', Education === sequelize.model('educations'));
 
 
-sequelize.sync({ force: true }) // Set force to true if you want to drop the tables first
+sequelize.sync({ force: false }) // Set force to true if you want to drop the tables first
     .then(() => {
         console.log('Database synchronized successfully.');
         process.exit();
